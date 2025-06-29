b8dce14b8cf0bf630baa169014c11eb22519b31d
Oli Holbourns <oliver.holbourns@gmail.com>
2025-06-29 20:54:42 +0200

    Writing a test for the ListParser (1st Story Card)

---
+++ src/main/java/VisualAlgorithmSimulator/ListParser.java
@@ -0,0 +1,11 @@
+package VisualAlgorithmSimulator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListParser {
+    public static List<Integer> parseValues(String values) {
+        //TODO  WRITE CLASS
+        return new ArrayList<>();
+    }
+}
---
+++ src/test/java/VisualAlgorithmSimulator/TestListParser.java
@@ -0,0 +1,22 @@
+package VisualAlgorithmSimulator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TestListParser {
+
+    @Test
+    public void TestValidList() {
+        ListParser listParser = new ListParser();
+        List<Integer> listOfValues = listParser.parseValues("1, 2, 5, 8, 10, 12");
+
+        //check that listOfValues is contains the same amount of numbers as the input
+        Assertions.assertEquals(6, listOfValues.size());
+
+        //check that the correct values have been inserted into listOfValues
+        Assertions.assertEquals(Arrays.asList(1, 2, 5, 8, 10, 12), listOfValues);
+    }
+}
